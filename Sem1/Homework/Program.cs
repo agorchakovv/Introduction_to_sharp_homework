b195d418cc771f6a00fcf1766d186dfd6810f2fe
agorchakovv <a.gorchakovv@gmail.com>
2022-10-03 17:30:56 +0600

    task 1

--- Sem1/Homework/Program.cs
+++ Sem1/Homework/Program.cs
@@ -1,4 +1,18 @@
-﻿Console.WriteLine("Input first number");
-Console.ReadLine();
+﻿/*
+//Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Input first number");
+int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Input second number");
-Console.ReadLine();
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+if (a > b)
+{
+    Console.WriteLine($"{a} more {b}");
+}
+else
+{
+    Console.WriteLine($"{b} more {a}");
+}
+
+*/
