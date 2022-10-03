d9372297f987d7304d790968a5b7c3c149ab5373
agorchakovv <a.gorchakovv@gmail.com>
2022-10-03 17:46:37 +0600

    task 2

--- Sem1/Homework/Program.cs
+++ Sem1/Homework/Program.cs
@@ -2,17 +2,40 @@
 //Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
 Console.WriteLine("Input first number");
-int a = Convert.ToInt32(Console.ReadLine());
+int numa = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Input second number");
-int b = Convert.ToInt32(Console.ReadLine());
+int numb = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine();
 if (a > b)
 {
-    Console.WriteLine($"{a} more {b}");
+    Console.WriteLine($"{numa} more {numb}");
 }
 else
 {
-    Console.WriteLine($"{b} more {a}");
+    Console.WriteLine($"{numb} more {numa}");
 }
+
+*/
+
+/*
+//Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Input first number");
+int numa = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input first number");
+int numb = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input first number");
+int numc = Convert.ToInt32(Console.ReadLine());
+
+int Max(int a, int b, int c)
+{
+    int max = a;
+    if(b > max) max = b;
+    if(c > max) max = c;
+    return max;
+}
+
+int max = Max(numa, numb, numc);
+Console.WriteLine("Maximum number " + max);
 
 */
