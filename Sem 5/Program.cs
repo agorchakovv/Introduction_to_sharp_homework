e99ceb56b9e327055dbbc1b08eb9a979444da236
agorchakovv <a.gorchakovv@gmail.com>
2022-10-22 20:36:14 +0600

    sem 5

---
+++ Sem 5/Program.cs
@@ -0,0 +1,42 @@
+﻿//Задача. Задать массив из 12 элементов, заполнить случайными числами от -9 до 9, найти сумму отриицательных элементов массива.
+
+// Заполнение массивва вводом пользователя
+int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+    Console.WriteLine("Creating array");
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"input a {i + 1} element of array: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    Console.WriteLine("Complete!");
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+
+int SumNegativeNum(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < 0)
+            sum += array[i];
+    }
+    return sum;
+}
+
+Console.WriteLine("input a number of elements: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] myArray = CreateArray(size);
+int result = SumNegativeNum(myArray);
+
+ShowArray(myArray);
+Console.WriteLine("Sum of a negative elements is " + result);
---
+++ Sem4/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задача. Заполнить массив единицами и нулями в случайном порядке и вывести на экран
+
+// Заполнить массив рандомными элементами
+
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] newArray = new int[size];
+
+    for (int i = 0; i < size; i++)
+        newArray[i] = new Random().Next(minValue, maxValue + 1);
+
+    return newArray;
+}
+
+//Распечатать массив
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+
+Console.WriteLine("Input size array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int min = 0;
+int max = 1;
+
+int[] array = CreateRandomArray(size, min, max);
+ShowArray(array);
